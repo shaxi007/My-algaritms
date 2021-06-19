cd0e064a140efcf672bd3890de38b58faeec5418
Teshayev Shaxboz <80866562+shaxi007@users.noreply.github.com>
2021-06-20 04:45:29 +0500

    Delete the smallest of the numbers.js

--- the smallest of the numbers.js
+++
@@ -1,9 +0,0 @@
-let nums=[-5,-8,7,-90,78,-6,12]
-
-let temporarily=nums[nums.length-1]
-for(let i=0;i<nums.length-1;++i){
-    if(temporarily>nums[i]){
-        temporarily=nums[i]
-    }
-}
-console.log(temporarily);
